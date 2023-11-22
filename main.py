a5e2cf1c1691d162a81c50ac0818344bd8bb57e4
N1ntend01 <151791965+N1ntend01@users.noreply.github.com>
2023-11-22 22:12:39 +0000

    the thing

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
-# Function that takes a string and returns a list of all the 
-# permutations of the string, shifting the positions of the 
-# characters in the string.
+# Define the function that takes a string and returns a list of
+# all the permutations of the string, shifting the positions of
+# the characters in the string.
 def caesar_cipher_brute_force_attack(ciphertext):
-    with open("output.txt", "w") as output_file:
-        for shift in range(26):
-            plaintext = ""
-            for char in ciphertext:
-                if char.isalpha():
-                    new_char = chr((ord(char.lower()) - ord('a') + shift) % 26 + ord('a'))
-                    if char.isupper():
-                        new_char = new_char.upper()
-                    plaintext += new_char
-                else:
-                    plaintext += char
-            output_file.write(f"Shift: {shift}, Plaintext: {plaintext}\n")
+  with open("output.txt", "w") as output_file:
+    for shift in range(26):
+      plaintext = ""
+      for char in ciphertext:
+        if char.isalpha():
+          new_char = chr((ord(char.lower()) - ord('a') + shift) % 26 +
+                         ord('a'))
+          if char.isupper():
+            new_char = new_char.upper()
+          plaintext += new_char
+        else:
+          plaintext += char
+      output_file.write(f"Shift: {shift}, Plaintext: {plaintext}\n")
+
+
+#
+# MAIN PROGRAM
+#
 
 # Read ciphertext from input file
 with open("input.txt", "r") as input_file:
-    ciphertext = input_file.read().strip()
+  ciphertext = input_file.read().strip()
 
 # Perform brute force attack and write results to output file
 caesar_cipher_brute_force_attack(ciphertext)
